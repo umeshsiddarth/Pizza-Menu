0e26cb0dd17ebfe0fbd15f775baa66d23533baac
Umesh Siddarth <umeshsiddarth@gmail.com>
2025-05-17 00:54:09 +0530

    Conditional Rendering, props, react fragments, and other react basics implemented

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       {/* <h1>Hello REACT!!!</h1> */}
       <Header />
       <Menu />
@@ -57,40 +58,99 @@ function App() {
   );
 }
 
-function Pizza() {
-  return (
-    <div>
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricota cheese.</p>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-    </div>
-  );
-}
-
 const Header = () => {
-  return <h1>Fast React Pizza Co.</h1>;
+  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
 };
 
 const Menu = () => {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
+      {/* <Pizza
+        name="Pizza 1"
+        ingredients="Ing1, ing2"
+        price={10} // Entering number instead of string
+        photoName="pizzas/spinaci.jpg"
+      /> */}
+      {numPizzas > 0 ? (
+        <>
+          {/* React Fragment */}
+          <p>
+            Authentic Italian cuisine. Stone Oven, All Organic and Delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              //Map is used bcoz it creates a new array. forEach won't work here bcoz it won't create a new array
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>Sorry! We ran out of Pizzas for today. Come back tomorrow!</p>
+      )}
+    </main>
   );
 };
 
-const Footer = () => {
+function Pizza({ pizzaObj }) {
+  // Using Destructuring instead of getting the whole props for improved readability
+
+  // if (pizzaObj.soldOut) return null;
+  // Based on multiple return conditional rendering.
   return (
-    <footer>{new Date().toLocaleTimeString()} We're currently open.</footer>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+      </div>
+    </li>
+  );
+}
+
+const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order openHour={openHour} closeHour={closeHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
+      )}
+    </footer>
   );
 
   // For education purpose on how hard it was to use createElement method earlier
   // return React.createElement("footer", null, "We're currently open");
 };
+
+function Order({ openHour, closeHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
