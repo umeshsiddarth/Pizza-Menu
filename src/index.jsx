744a03db3fe2b452279ee0c315a4d050729888ae
Umesh Siddarth <umeshsiddarth@gmail.com>
2025-05-16 17:45:36 +0530

    Pizza menu reat projectusing Vite

---
+++ src/index.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      {/* <h1>Hello REACT!!!</h1> */}
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricota cheese.</p>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+    </div>
+  );
+}
+
+const Header = () => {
+  return <h1>Fast React Pizza Co.</h1>;
+};
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open.</footer>
+  );
+
+  // For education purpose on how hard it was to use createElement method earlier
+  // return React.createElement("footer", null, "We're currently open");
+};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
